053024d1f916508dbaa4cc5f57c3f89a44704fca
MrFirewall <32974726+MrFirewall@users.noreply.github.com>
2025-12-05 17:28:33 +0100

    ✨ feat(rules): rename rules route to rules.index
    - change route name for rules index page from 'rules' to 'rules.index' to align with standard resource naming conventions.

--- routes/web.php
+++ routes/web.php
@@ -79,7 +79,7 @@ Route::middleware('auth.cfx')->group(function () {
     // Standard-Ressourcen
     Route::resource('reports', ReportController::class);
     Route::resource('citizens', CitizenController::class);
-    Route::get('/rules', [RuleController::class, 'index'])->name('rules');
+    Route::get('/rules', [RuleController::class, 'index'])->name('rules.index');
     Route::get('/rules', [RuleController::class, 'create'])->name('rules.create');
 
     // Dienststatus
